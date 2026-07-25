29f6a69507112f4c0a2ff1e04ab80fef0ce68ec1
Yilin Huang <kenshing@foxmail.com>
2026-07-25 14:02:13 +0800

    🐛 fix black screen on Pages: drop BrowserRouter (route '/' mismatches subpath)

--- witch-poison/src/App.tsx
+++ witch-poison/src/App.tsx
@@ -1,10 +1,8 @@
-import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 
+// 单页面游戏：直接渲染，不用路由。
+// 此前 BrowserRouter 只匹配 "/"，部署到子路径（如 GitHub Pages 的
+// /games/witch-poison/）时路由失配导致整页空白（黑屏）。
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  )
+  return <Home />
 }
